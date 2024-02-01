cb8b7ff5c06fb29dfc1dc446b5c912af70b43487
cs-shivaji-kolape <skolape@fortinet.com>
2024-02-01 21:15:18 +0530

    Fixed auth code exipred issue for on update connector config

--- microsoft-graph/connector.py
+++ microsoft-graph/connector.py
@@ -5,9 +5,8 @@
   Copyright end """
 
 from connectors.core.connector import Connector, get_logger, ConnectorError
-from .operations import operations, _check_health, API_VERSION
-
-
+from .operations import operations, _check_health, API_VERSION, AUTH_BEHALF_OF_USER
+from connectors.core.utils import update_connnector_config
 logger = get_logger('microsoft_graph')
 
 
@@ -28,3 +27,18 @@ class MicrosoftGraph(Connector):
         config['connector_info'] = {"connector_name": self._info_json.get('name'),
                                     "connector_version": self._info_json.get('version')}
         return _check_health(config)
+
+    def on_update_config(self, old_config, new_config, active):
+        connector_info = {"connector_name": self._info_json.get('name'),
+                          "connector_version": self._info_json.get('version')}
+
+        if new_config.get('auth_type', '') == AUTH_BEHALF_OF_USER:
+            old_auth_code = old_config.get('code')
+            new_auth_code = new_config.get('code')
+            if old_auth_code != new_auth_code:
+                new_config.pop('accessToken', '')
+            else:
+                new_config['accessToken'] = old_config.get('accessToken')
+                new_config['expiresOn'] = old_config.get('expiresOn')
+            update_connnector_config(connector_info['connector_name'], connector_info['connector_version'], new_config,
+                                 new_config['config_id'])
